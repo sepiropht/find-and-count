bcf217ba67ac27d1c790599feae0e603925987f2
sepiropht <sergembotta@mailoo.org>
2018-11-12 01:47:23 +0100

    it working!

--- src/file_tree.rs
+++ src/file_tree.rs
@@ -1,37 +1,36 @@
-use std::ffi::{OsString, OsStr};
+use std::ffi::{OsStr, OsString};
 use std::io::{self, Write};
 use std::path::Path;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+extern crate colored;
 
+use colored::*;
 const VERTICAL_CHAR: char = '│';
 const HORIZONTAL_STR: &'static str = "├──";
 const LAST_HORIZONTAL_STR: &'static str = "└──";
 const REPLACEMENT_CHAR: char = '?';
 
-#[derive(Debug,PartialEq,Eq)]
+
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct FileTree {
     name: OsString,
+    frame_work: String,
     childs: Vec<FileTree>,
 }
 
-fn print_line<W: Write>(output: &mut W, lasts: &[bool], name: &OsStr) -> io::Result<()> {
-    let name: String = name.to_string_lossy()
+fn print_line<W: Write>(output: &mut W, lasts: &[bool], name: &OsStr, frame_work: &str) -> io::Result<()> {
+    let name: String = name
+        .to_string_lossy()
         .chars()
-        .map(|c| {
-            if c.is_control() {
-                REPLACEMENT_CHAR
-            } else {
-                c
-            }
-        })
+        .map(|c| if c.is_control() { REPLACEMENT_CHAR } else { c })
         .collect();
 
     if lasts.len() > 0 {
         for last in &lasts[..lasts.len() - 1] {
-            let c = if *last {
-                ' '
-            } else {
-                VERTICAL_CHAR
-            };
+            let c = if *last { ' ' } else { VERTICAL_CHAR };
             write!(output, "{}   ", c)?;
         }
         if *lasts.last().unwrap() {
@@ -40,15 +39,35 @@ fn print_line<W: Write>(output: &mut W, lasts: &[bool], name: &OsStr) -> io::Res
             write!(output, "{} ", HORIZONTAL_STR,)?;
         }
     }
+    if frame_work == "react" {
+         writeln!(output, "{} : {}", name, frame_work.color("blue"))?;
 
-    writeln!(output, "{}", name)?;
+    } else {
+        writeln!(output, "{} : {}", name, frame_work.color("red"))?; 
+    }
+   
 
     Ok(())
 }
 
+fn extract_framework(path: &Path) -> std::io::Result<String> {
+    let file = File::open(path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    if contents.contains("react") {
+        return Ok("react".to_string()); 
+    }  
+    if contents.contains("angular") {
+     return Ok("angular".to_string());
+    } else {
+        return Ok("".to_string());
+    }
+}
+
 impl FileTree {
     pub fn print<W: Write>(&self, out: &mut W, lasts: &mut Vec<bool>) -> io::Result<()> {
-        print_line(out, &lasts[..], &*self.name)?;
+        print_line(out, &lasts[..], &*self.name, &*self.frame_work)?;
         lasts.push(false);
         for (i, child) in self.childs.iter().enumerate() {
             if i + 1 == self.childs.len() {
@@ -60,42 +79,48 @@ impl FileTree {
         Ok(())
     }
 
-    fn add<'a, I: Iterator<Item = &'a OsStr>>(&mut self, name_iter: &mut I) {
+    fn add<'a, I: Iterator<Item = &'a OsStr>>(&mut self, name_iter: &mut I, abs_path: String) {
         if let Some(c) = name_iter.next() {
             let mut found = false;
             for child in &mut self.childs {
                 if &*child.name == c {
-                    child.add(name_iter);
+                    child.add(name_iter, abs_path.clone());
                     found = true;
                     break;
                 }
             }
 
             if !found {
+                
                 let new_child = FileTree {
                     name: c.to_os_string(),
+                    frame_work: extract_framework(&Path::new(&abs_path)).expect("putain fonctionne !!!"),
                     childs: vec![],
                 };
+                
                 self.childs.push(new_child);
-                self.childs.last_mut().unwrap().add(name_iter);
+                self.childs.last_mut().unwrap().add(name_iter, abs_path);
             }
         }
     }
 }
 
 pub fn make_trees<I, O>(input: &mut I) -> Vec<FileTree>
-    where I: Iterator<Item = O>,
-          O: AsRef<OsStr>
+where
+    I: Iterator<Item = O>,
+    O: AsRef<OsStr>,
 {
     let mut pseudo_root = FileTree {
         name: OsString::new(),
+        frame_work: "".to_string(),
         childs: vec![],
     };
 
     for line in input {
         let path = Path::new(&line);
+        //println!("merde {:?}", path);
         let mut components = path.components().map(|c| c.as_os_str());
-        pseudo_root.add(&mut components);
+        pseudo_root.add(&mut components, path.to_string_lossy().to_string());
     }
 
     pseudo_root.childs
@@ -103,8 +128,8 @@ pub fn make_trees<I, O>(input: &mut I) -> Vec<FileTree>
 
 #[cfg(test)]
 mod tests {
-    use super::{FileTree, make_trees, print_line};
-    use std::ffi::{OsString, OsStr};
+    use super::{make_trees, print_line, FileTree};
+    use std::ffi::{OsStr, OsString};
 
     fn test_single_tree_creation(lines: &[&str], expected_tree: FileTree) {
         let trees = make_trees(&mut lines.iter());
@@ -207,7 +232,6 @@ mod tests {
         let mut output3 = vec![];
         print_line(&mut output3, &[true, false, false], name).unwrap();
         assert_eq!("    │   ├── abc?def\n".as_bytes(), &*output3);
-
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use std::ffi::OsString;
 use std::fs;
 use std::io::{self, BufRead};
 use std::path::Path;
-
+//react
 mod file_tree;
 
 fn main() -> std::io::Result<()> {
     let mut files = vec![];
-    crawl_dir(&Path::new("."), &mut files)?;
-    println!("{:?}", files);
-    println!(
-        "il y a {} fichiers dans ce repertoire et ses sous repertoires",
-        files.len()
-    );
+    crawl_dir(&Path::new("./src/"), &mut files)?;
+    // println!("{:?}", files);
+    // println!(
+    //     "il y a {} fichiers dans ce repertoire et ses sous repertoires",
+    //     files.len()
+    // );
     let mut input = files.iter().map(|s| OsString::from(s));
     let trees = file_tree::make_trees(&mut input);
 
@@ -28,8 +28,13 @@ fn crawl_dir(dir: &Path, files: &mut Vec<String>) -> std::io::Result<()> {
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let path = entry.path();
-        if path.is_file() {
-            files.push(path.to_string_lossy().to_string());
+        if path.is_file()  {
+            if path.extension().unwrap() == "js" {
+              println!("{:?}", path);
+              files.push(path.to_string_lossy().to_string());
+            } else {
+                println!("do nothing");
+            }
         } else {
             crawl_dir(&path, files)?;
         }
